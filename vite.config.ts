73d557e020418ceab6e726ff1ce5b6e9943f8377
Danalizi-Erőss Örs <danalizieors@gmail.com>
2026-02-25 21:18:41 +0200

    add deploy configuration for Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { cloudflare } from "@cloudflare/vite-plugin"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => {
       allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : undefined,
     },
     plugins: [
+      cloudflare({ viteEnvironment: { name: "ssr" } }),
       tsConfigPaths(),
       tanstackStart(),
       viteReact(),
